b0370d0d2d35783dede0830068011a7908965d11
Bruna Ferreira <brunagaby8@gmail.com>
2025-07-16 19:22:33 -0300

    Implementação da classe User em Model. Implementação da classe UserRole que determina os papéis "USER" e "ADMIN"

---
+++ backend/plataforma.mentoria/src/main/java/br/edu/ufape/plataforma/mentoria/model/User.java
@@ -0,0 +1,108 @@
+package br.edu.ufape.plataforma.mentoria.model;
+
+import java.util.Collection;
+import java.util.List;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import br.edu.ufape.plataforma.mentoria.util.UserRole;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.SequenceGenerator;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "users")
+public class User implements UserDetails {
+    
+    @Id
+    @SequenceGenerator(name="user_id_seq", sequenceName="user_id_seq", allocationSize=1)
+    @GeneratedValue(generator="user_id_seq", strategy=GenerationType.SEQUENCE)
+    @Column(name="id", updatable=false)
+    private Long id;
+    
+    @Column(nullable = false, unique = true)
+    private String email;
+    
+    @Column(nullable = false)
+    private String password;
+    
+    @Column(nullable = false)
+    private UserRole role;
+
+    @Column (nullable = false)
+    private String name;
+
+    public User() {
+    }
+
+    public User(String name, String email, String password, UserRole role) {
+        this.name = name;
+        this.email = email;
+        this.password = password;
+        this.role = role;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    @Override
+    public String getUsername() {
+        return email;
+    }
+
+    @Override
+    public String getPassword() {
+        return password;
+    }
+
+    public UserRole getRole() {
+        return role;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public void setRole(UserRole role) {
+        this.role = role;
+    }
+
+    public void setName(String name){
+        this.name = name;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        if (this.role == UserRole.ADMIN) {
+            return List.of(new SimpleGrantedAuthority("ROLE_ADMIN"), 
+             new SimpleGrantedAuthority("ROLE_USER"));
+        } else {
+            return List.of(new SimpleGrantedAuthority("ROLE_USER"));
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "User [nome=" + name + "email=" + email + ", id=" + id + ", password=" + password + ", role=" + role + "]";
+    }
+}
+
