8a7f37585f6db7be581e62d9226003cc3f3c034a
Bruna Ferreira <brunagaby8@gmail.com>
2025-07-16 20:01:43 -0300

    Alteração para no SecurityConfig para que qualquer request seja autorizado, temporário enquando ainda não temos rotas.

--- backend/plataforma.mentoria/src/main/java/br/edu/ufape/plataforma/mentoria/security/SecurityConfig.java
+++ backend/plataforma.mentoria/src/main/java/br/edu/ufape/plataforma/mentoria/security/SecurityConfig.java
@@ -26,11 +26,11 @@ public class SecurityConfig {
         return httpSecurity.csrf(csrf -> csrf.disable())
             .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
             .authorizeHttpRequests(authorize -> authorize
-            .requestMatchers(HttpMethod.POST, "/").permitAll()
+            .anyRequest().permitAll()
             //.requestMatchers(HttpMethod.POST, "/auth/register").permitAll()
             //.requestMatchers("/location/**", "/booking/**").hasRole("ADMIN")
             //To-do: substituir as rodas, quando estas estiverem devidamente implementadas
-            .anyRequest().authenticated()
+            //.anyRequest().authenticated()
             )
             .addFilterBefore(securityFilter, UsernamePasswordAuthenticationFilter.class)
             .build();
