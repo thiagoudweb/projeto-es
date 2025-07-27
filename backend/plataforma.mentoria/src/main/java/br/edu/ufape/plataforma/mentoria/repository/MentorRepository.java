b5beee2d056d7d09bfb5290810cbc5f8fca34987
gabrielZZ231 <gabriel1235897@gmail.com>
2025-07-27 16:59:07 -0300

    [feat][backend]: T3 - Criar repositório MentorRepository

---
+++ backend/plataforma.mentoria/src/main/java/br/edu/ufape/plataforma/mentoria/repository/MentorRepository.java
@@ -0,0 +1,11 @@
+package br.edu.ufape.plataforma.mentoria.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import br.edu.ufape.plataforma.mentoria.model.Mentor;
+
+
+@Repository
+public interface MentorRepository extends JpaRepository<Mentor, Long> {
+
+}
