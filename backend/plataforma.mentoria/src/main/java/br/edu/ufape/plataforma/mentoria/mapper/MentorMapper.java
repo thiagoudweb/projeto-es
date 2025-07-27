efa3198bd7d9c5ad8e799873aa3c38e62e0c1bb8
gabrielZZ231 <gabriel1235897@gmail.com>
2025-07-27 17:03:54 -0300

    T8 - Criar serviço MentorService

---
+++ backend/plataforma.mentoria/src/main/java/br/edu/ufape/plataforma/mentoria/mapper/MentorMapper.java
@@ -0,0 +1,37 @@
+package br.edu.ufape.plataforma.mentoria.mapper;
+
+import org.springframework.stereotype.Component;
+
+import br.edu.ufape.plataforma.mentoria.dto.MentorDTO;
+import br.edu.ufape.plataforma.mentoria.model.Mentor;
+
+@Component
+public class MentorMapper {
+    public Mentor toEntity(MentorDTO mentorDTO) {
+        Mentor mentor = new Mentor();
+        mentor.setFullName(mentorDTO.getFullName());
+        mentor.setCpf(mentorDTO.getCpf());
+        mentor.setBirthDate(mentorDTO.getBirthDate());
+        mentor.setCourse(mentorDTO.getCourse());
+
+        mentor.setProfessionalSummary(mentorDTO.getProfessionalSummary());
+        mentor.setAffiliationType(mentorDTO.getAffiliationType());
+        mentor.setSpecializations(mentorDTO.getSpecializations());
+
+        return mentor;
+    }
+
+    public MentorDTO toDto(Mentor mentor) {
+        MentorDTO mentorDTO = new MentorDTO();
+        mentorDTO.setFullName(mentor.getFullName());
+        mentorDTO.setCpf(mentor.getCpf());
+        mentorDTO.setBirthDate(mentor.getBirthDate());
+        mentorDTO.setCourse(mentor.getCourse());
+
+        mentorDTO.setProfessionalSummary(mentor.getProfessionalSummary());
+        mentorDTO.setAffiliationType(mentor.getAffiliationType());
+        mentorDTO.setSpecializations(mentor.getSpecializations());
+
+        return mentorDTO;
+    }
+}
