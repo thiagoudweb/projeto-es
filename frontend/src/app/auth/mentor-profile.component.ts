25355596a87a2cc453c636b2c477afaa4c328017
Thiago Silva <thhzmonster@gmail.com>
2025-07-28 16:08:39 -0300

    [feat][frontend]: T14 - Criar um modal para editar as informações do perfil do mentor

---
+++ frontend/src/app/auth/mentor-profile.component.ts
@@ -0,0 +1,29 @@
+import { Component, Input } from '@angular/core';
+import { NgIf } from '@angular/common';
+import { Mentor } from './mentor';
+import { EditMentorProfileModalComponent } from './edit-mentor-profile-modal/edit-mentor-profile-modal.component';
+
+@Component({
+  selector: 'app-mentor-profile',
+  standalone: true,
+  imports: [NgIf, EditMentorProfileModalComponent],
+  templateUrl: './mentor-profile.component.html',
+  styleUrls: ['./mentor-profile.component.css']
+})
+export class MentorProfileComponent {
+  @Input() mentor!: Mentor;
+  showEditModal = false;
+
+  openEditModal() {
+    this.showEditModal = true;
+  }
+
+  closeEditModal() {
+    this.showEditModal = false;
+  }
+
+  saveMentorProfile(updatedMentor: Mentor) {
+    this.mentor = updatedMentor;
+    this.closeEditModal();
+  }
+}
