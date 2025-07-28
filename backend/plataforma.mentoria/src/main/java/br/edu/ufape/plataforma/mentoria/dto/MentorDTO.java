39e0f825e53e1492eb58e2b3e879e232c03d97f3
raylandson <www.raylandsoncs@gmail.com>
2025-07-28 16:55:43 -0300

    [fix][backend]: Remover importações não utilizadas

--- backend/plataforma.mentoria/src/main/java/br/edu/ufape/plataforma/mentoria/dto/MentorDTO.java
+++ backend/plataforma.mentoria/src/main/java/br/edu/ufape/plataforma/mentoria/dto/MentorDTO.java
@@ -4,7 +4,6 @@ import java.time.LocalDate;
 import java.util.List;
 import br.edu.ufape.plataforma.mentoria.enums.AffiliationType;
 import br.edu.ufape.plataforma.mentoria.enums.Course;
-import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
