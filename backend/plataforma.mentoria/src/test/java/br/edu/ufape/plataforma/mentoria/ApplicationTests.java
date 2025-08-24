ff5f0053bd3020e250aed585ddc769cc0471f1ea
raylandson <www.raylandsoncs@gmail.com>
2025-08-24 18:10:16 -0300

    📝 docs(tests): adicionar comentários explicativos no teste de contexto

--- backend/plataforma.mentoria/src/test/java/br/edu/ufape/plataforma/mentoria/ApplicationTests.java
+++ backend/plataforma.mentoria/src/test/java/br/edu/ufape/plataforma/mentoria/ApplicationTests.java
@@ -9,6 +9,9 @@ class ApplicationTests {
 	@Test
 	void contextLoads() {
 		// this test verifies if the Spring application context loads successfully
+		// No need explicit test logic / assertions
+		// Just loading the context is enough
+		// it will pass if the context loads successfully
 	}
 
 }
