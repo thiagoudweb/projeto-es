fa54244a597c0f914c7ae63441c59a545e58a4e2
Bruna Ferreira <brunagaby8@gmail.com>
2025-09-06 19:43:22 -0300

    [backend][cleanup] Remove imports não utlizados

--- backend/plataforma.mentoria/src/main/java/br/edu/ufape/plataforma/mentoria/repository/UserRepository.java
+++ backend/plataforma.mentoria/src/main/java/br/edu/ufape/plataforma/mentoria/repository/UserRepository.java
@@ -1,5 +1,7 @@
 package br.edu.ufape.plataforma.mentoria.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import br.edu.ufape.plataforma.mentoria.model.User;
